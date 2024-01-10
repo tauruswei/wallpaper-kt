d489e30ff91cf769ae772a1ed7f849fbfcd8ccbc
王进 <513894357@qq.com>
2024-01-10 23:53:19 +0800

    萌宠大集合案例开始

--- uniappDemo3/pages/requestDemo/requestDemo.vue
+++ uniappDemo3/pages/requestDemo/requestDemo.vue
@@ -1,20 +1,79 @@
 <template>
-	<view class="container">
+	<view class="container">			
+		<view class="layout">
+			<view class="box" v-for="(item,index) in pets" :key="item._id">
+				<view class="pic">
+					<image lazy-load :src="item.url" mode="widthFix" @click="onPreview(index)"></image>
+				</view>
+				<view class="text">{{item.content}}</view>
+				<view class="author">—— {{item.author}}</view>
+			</view>
+		</view>		
 		
 	</view>
 </template>
 
 <script setup>
-uni.request({
-	url:"https://tea.qingnian8.com/tools/taoShow",
-	header:{
-		'access-key':'xxm123321@@'
-	}
-}).then(res=>{
-	console.log(res);
-})
+import {ref} from "vue";
+const pets = ref([]); 
+
+const onPreview = function (index){
+	let urls = pets.value.map(item=>item.url);
+	
+	uni.previewImage({
+		current:index,
+		urls
+	}) 
+}
+
+
+//发送网络请求
+function network(){
+	uni.request({
+		url:"https://tea.qingnian8.com/tools/petShow",
+		data:{
+			size:10
+		},
+		header:{
+			"access-key":"xxm123321@#"
+		}
+	}).then(res=>{
+		console.log(res.data.data);
+		pets.value = res.data.data
+	})
+}
+
+network();
+
+
 </script>
 
 <style lang="scss" scoped>
-
+.container{
+	.layout{
+		padding:50rpx;
+		.box{
+			margin-bottom:60rpx;
+			box-shadow: 0 10rpx 40rpx rgba(0,0,0,0.08);
+			border-radius: 15rpx;
+			overflow: hidden;
+			.pic{
+				image{
+					width: 100%;
+				}
+			}
+			.text{
+				padding:30rpx;
+				color:#333;
+				font-size: 36rpx;
+			}
+			.author{
+				padding:0 30rpx 30rpx;
+				text-align: right;
+				color:#888;
+				font-size: 28rpx;
+			}
+		}
+	}
+}
 </style>
