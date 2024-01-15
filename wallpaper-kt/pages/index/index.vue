5d9a1def8ad521888b59b4494a27560709a45578
王进 <513894357@qq.com>
2024-01-15 23:59:43 +0800

    布局海报区和轮播区

---
+++ wallpaper-kt/pages/index/index.vue
@@ -0,0 +1,100 @@
+<template>
+	<view class="homeLayout">
+		<view class="banner">
+			<swiper circular indicator-dots indicator-color="rgba(255,255,255,0.5)" 
+			indicator-active-color="#fff" autoplay>
+				<swiper-item>
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner2.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner3.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="sound-filled" size="20" color="#28b389"></uni-icons>
+				<text class="text">公告</text>
+			</view>
+			<view class="center">
+				<swiper vertical autoplay interval="1500" duration="300" circular>
+					<swiper-item v-for="item in 4">文字内容文字内容文字内容文字内容文字内容文字内容</swiper-item>
+				</swiper>
+			</view>
+			<view class="right">
+				<uni-icons type="right" size="16" color="#333"></uni-icons>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.homeLayout{
+	.banner{
+		width: 750rpx;
+		padding:30rpx 0;
+		swiper{
+			width: 750rpx;
+			height: 340rpx;
+			&-item{
+				width: 100%;
+				height: 100%;
+				padding:0 30rpx;
+				image{
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+			}
+		}
+	}
+	.notice{
+		width: 690rpx;
+		height: 80rpx;
+		line-height: 80rpx;
+		background: #f9f9f9;
+		margin: 0 auto;
+		border-radius: 80rpx;
+		display: flex;
+		.left{
+			width: 140rpx;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			.text{
+				color:#28b389;
+				font-weight: 600;
+				font-size: 28rpx;
+			}
+		}
+		.center{
+			flex:1;
+			swiper{
+				height: 100%;
+				&-item{
+					height: 100%;
+					font-size: 30rpx;
+					color:#666;
+					overflow: hidden;
+					white-space: nowrap;
+					text-overflow: ellipsis;
+				}
+			}
+		}
+		.right{
+			width: 70rpx;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+	}
+}
+</style>
